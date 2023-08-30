d0e7d37137b038049523c2cf8c29a5fd26825c85
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-08-30 18:37:59 +0600

    dbConnected

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
+const pgp = require('pg-promise')()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express()
+const port = process.env.PORT || 3000
+
+const connection = {
+  host: process.env.SUPABASEHOST,
+  port: process.env.SUPABASEPORT,
+  database: process.env.SUPABASEDATABASE,
+  user: process.env.SUPABASEUSER,
+  password: process.env.SUPABASEPASSWORD,
+  ssl: { rejectUnauthorized: false },
+};
+const db = pgp(connection);
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await db.any('SELECT * FROM dummy');
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
