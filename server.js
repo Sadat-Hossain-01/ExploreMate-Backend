3f0d85cd3f06f8a35a03724d6882a69062c6f5e8
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-09-03 01:50:55 +0600

    FirstPage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const pgp = require('pg-promise')()
 
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 3000
 
 const connection = {
@@ -17,7 +18,23 @@ const db = pgp(connection);
 
 app.get('/recom', async (req, res) => {
   try {
-    const data = await db.any('SELECT * FROM "City"');
+    const data = await db.any('select C.name as name, A.name as state, B.name as country \
+                                from "City" as C, "Country" as B, "State" as A \
+                                where C.s_id = A.id \
+                                and B.id = A.c_id');
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/city', async (req, res) => {
+  try {
+    const city = req.body['city'];
+    const data = await db.any('select * from "Destination" \
+                                where city_id = (select id from "City" \
+                                where name = $1);', [city]);
     res.json(data);
   } catch (error) {
     console.error(error);
