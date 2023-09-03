87ac366b5704b6bae0fda3e459f8da806ce2020b
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-09-03 15:37:24 +0600

    HotelRestAdded

--- routes/destination.js
+++ routes/destination.js
@@ -33,4 +33,34 @@ router.post('/activity', async (req, res) => {
   }
 })
 
+router.post('/hotel', async (req, res) => {
+  try {
+    const city = req.body['destination'];
+    const data = await db.any('select * \
+                              from "AvailableHotel" as A, "Hotel" as B \
+                              where A.hotel_id = B.id \
+                              and destination_id = (select id from "Destination" \
+                              where name = $1)', [city]);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+router.post('/restaurant', async (req, res) => {
+  try {
+    const city = req.body['destination'];
+    const data = await db.any('select * \
+                              from "AvailableRestaurant" as A, "Restaurant" as B \
+                              where A.restaurant_id = B.id \
+                              and destination_id = (select id from "Destination" \
+                              where name = $1)', [city]);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
 module.exports = router;
