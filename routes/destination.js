30ad3f5e4c2009c21c9b800cb1273faa0f6fb15b
Shattik <1905008@ugrad.cse.buet.ac.bd>
2023-09-05 18:55:17 +0600

    facingProblem

--- routes/destination.js
+++ routes/destination.js
@@ -5,12 +5,16 @@ const db = require('../db');
 
 router.post('/event', async (req, res) => {
   try {
-    const city = req.body['destination'];
-    const data = await db.any('select * \
-                              from "AvailableEvent" as A, "Event" as B \
-                              where A.event_id = B.id \
-                              and destination_id = (select id from "Destination" \
-                              where name = $1)', [city]);
+    const destination = req.body['destinations'];
+    let query = 'select * from "AvailableEvent" as A, "Event" as B where A.event_id = B.id and A.destination_id in (select id from "Destination" where name in (';
+    for(let i = 0; i < destination.length; i++) {
+      query += `'${destination[i]}'`;
+      if(i != destination.length - 1) {
+        query += ', ';
+      }
+    }
+    query += '));';
+    const data = await db.any(query);
     res.json(data);
   } catch (error) {
     console.error(error);
@@ -20,13 +24,17 @@ router.post('/event', async (req, res) => {
 
 router.post('/activity', async (req, res) => {
   try {
-    const city = req.body['destination'];
-    const data = await db.any('select * \
-                              from "AvailableActivity" as A, "Activity" as B \
-                              where A.activity_id = B.id \
-                              and destination_id = (select id from "Destination" \
-                              where name = $1)', [city]);
-    res.json(data);
+    const destination = req.body['destinations'];
+    let query = 'select * from "AvailableActivity" as A, "Activity" as B where A.activity_id = B.id and A.destination_id in (select id from "Destination" where name in (';
+    for(let i = 0; i < destination.length; i++) {
+      query += `'${destination[i]}'`;
+      if(i != destination.length - 1) {
+        query += ', ';
+      }
+    }
+    query += '));';
+    console.log(query);
+    const data = await db.any(query);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -35,12 +43,16 @@ router.post('/activity', async (req, res) => {
 
 router.post('/hotel', async (req, res) => {
   try {
-    const city = req.body['destination'];
-    const data = await db.any('select * \
-                              from "AvailableHotel" as A, "Hotel" as B \
-                              where A.hotel_id = B.id \
-                              and destination_id = (select id from "Destination" \
-                              where name = $1)', [city]);
+    const destination = req.body['destinations'];
+    let query = 'select * from "AvailableHotel" as A, "Hotel" as B where A.hotel_id = B.id and A.destination_id in (select id from "Destination" where name in (';
+    for(let i = 0; i < destination.length; i++) {
+      query += `'${destination[i]}'`;
+      if(i != destination.length - 1) {
+        query += ', ';
+      }
+    }
+    query += '));';
+    const data = await db.any(query);
     res.json(data);
   } catch (error) {
     console.error(error);
@@ -50,12 +62,16 @@ router.post('/hotel', async (req, res) => {
 
 router.post('/restaurant', async (req, res) => {
   try {
-    const city = req.body['destination'];
-    const data = await db.any('select * \
-                              from "AvailableRestaurant" as A, "Restaurant" as B \
-                              where A.restaurant_id = B.id \
-                              and destination_id = (select id from "Destination" \
-                              where name = $1)', [city]);
+    const destination = req.body['destinations'];
+    let query = 'select * from "AvailableRestaurant" as A, "Restaurant" as B where A.restaurant_id = B.id and A.destination_id in (select id from "Destination" where name in (';
+    for(let i = 0; i < destination.length; i++) {
+      query += `'${destination[i]}'`;
+      if(i != destination.length - 1) {
+        query += ', ';
+      }
+    }
+    query += '));';
+    const data = await db.any(query);
     res.json(data);
   } catch (error) {
     console.error(error);
